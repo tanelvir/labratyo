ab8782dcfdb16c23e3980920166e99bf2404e611
Taneli Virkkala <taneli.virkkala@helsinki.fi>
2012-01-20 13:59:04 +0200

    sasasasas

--- src/Labratyo.java
+++ src/Labratyo.java
@@ -13,7 +13,7 @@ public class Labratyo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("COmmittedDDDDD");
     
     }
 }
